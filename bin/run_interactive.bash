360149d80b51a5d91b41aec3195efb0b3d94bbec
B. Lachele Foley <lachele@gmail.com>
2026-01-12 03:00:41 -0500

    Made interactive run nicer.

--- bin/run_interactive.bash
+++ bin/run_interactive.bash
@@ -18,23 +18,12 @@ if [ ! -d './deps/gems/External/MD_Utils/' ] ; then
 	exit 1
 fi
 
-export RUN_COMMAND='tail -f /dev/null'
 export CONTAINER_NAME="${CONTAINER_NAME}_interactive"
 
 # Second, run docker compose up.
-COMMAND="docker compose --file ${DOCKER_COMPOSE_RUN_FILE} up -d delegator"
-( ${COMMAND} ) || {  print_error_and_exit "ERROR UPPING RUN interactive IMAGE" ; } 
-echo "container is up"
+COMMAND="docker compose --file ${DOCKER_COMPOSE_RUN_FILE} run --rm delegator /bin/bash"
+( ${COMMAND} ) || {  print_error_and_exit "ERROR RUNNING DELEGATOR interactive IMAGE" ; } 
 
-COMMAND="docker compose --file ${DOCKER_COMPOSE_RUN_FILE}  exec -it delegator /bin/bash"
-#COMMAND="docker compose --file ${DOCKER_COMPOSE_RUN_FILE}  exec -it ${CONTAINER_NAME} /bin/bash"
-( ${COMMAND} ) || {  echo  "ERROR attaching to RUN interactive IMAGE" ; }
-
-echo "exiting container and stopping service"
-
-# Fourth, once done compiling then run docker compose down.
-COMMAND="docker compose --file ${DOCKER_COMPOSE_RUN_FILE} down"
-( ${COMMAND} ) || {  print_error_and_exit "ERROR DOWNING RUN interactive IMAGE" ; }
 
 # EXIT_SUCCESS
 exit 0
