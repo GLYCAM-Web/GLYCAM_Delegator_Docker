702ef20be2bb3a1096689dead8be1b867b66e483
B. Lachele Foley <lachele@gmail.com>
2026-03-05 08:35:24 -0500

    repo management is working as expected

--- bin/manage_repos.bash
+++ bin/manage_repos.bash
@@ -24,8 +24,6 @@ Follow progress by examining:
    ${STATUSFILE}
 """
 
-WorkDir="$(pwd)"
-
 ## This can go away after a while. It is only needed by folks who used the very earliest versions of the code.
 ## Submodules turned out to be a bit more hassle than just using sub-repos.
 bash bin/special/move_submodules_to_sub-branches.bash
@@ -37,10 +35,11 @@ if [ "${result}" != "0" ] ; then
 	exit 1
 fi
 
-for repo in ${Repos[@]} ; do
+for repo in "${Repos[@]}" ; do
         echo "Working on this repository: ${repo}"
-        parent="${Repo_Parent[${repo}]}"
-        if [ "${parent}" == "None" ] ; then
+        theParent="${Repo_Parent[${repo}]}"
+	echo "the parent is ${theParent}"
+        if [ "${theParent}" == "None" ] ; then
                 if [ ! -d "${Repo_Directory[${repo}]}/.git" ] ; then
                         COM="git clone -b ${Repo_Branch[${repo}]} ${Repo_URL[${repo}]} ${Repo_Directory[${repo}]}"
                         rclr "About to clone parent repo: ${repo}" "${COM}" "Cloning ${repo}"
@@ -48,37 +47,31 @@ for repo in ${Repos[@]} ; do
 
 		targetHash="${Repo_Target_Hash[${repo}]}"
 		if [ "${targetHash}" == "None" ] ; then
-			cd "${WorkDir}"
 			continue  
 		fi
 
-		cd "${Repo_Directory[${repo}]}"
-		theHash="$(git rev-parse HEAD)"
+		theHash="$(cd ${Repo_Directory[${repo}]} && git rev-parse HEAD )"
 		if [ "${theHash}" != "${targetHash}" ] ; then
-			COM="git revert ${targetHash}"
+			COM="( cd ${Repo_Directory[${repo}]} && git revert ${targetHash} --no-edit )"
 			rclr "Ensuring that the repo is on the target hash." "${COM}" "Target hash setting"
 		fi
         else
-		parentDir="${Repo_Directory[${parent}]}"
+		parentDir="${Repo_Directory[${theParent}]}"
                 if [ ! -d "${parentDir}/.git" ] ; then
-                        print_error_and_exit "The parent repo should already exist. It is: ${Repo_Directory[${parent}]}"
+                        print_error_and_exit "The parent repo should already exist. It is: ${Repo_Directory[${theParent}]}"
                 fi
-		cd ${parentDir}
-                COM="git clone -b ${Repo_Branch[${repo}]} ${Repo_URL[${repo}]} ${Repo_Directory[${repo}]}"
-                rclr "About to clone the sub repo ${repo} of parent ${parent}" "${COM}" "Cloning ${repo} as sub repo"
+		COM="( cd ${parentDir} && git clone -b ${Repo_Branch[${repo}]} ${Repo_URL[${repo}]} ${Repo_Directory[${repo}]} )"
+                rclr "About to clone the sub repo ${repo} of parent ${theParent}" "${COM}" "Cloning ${repo} as sub repo"
 
 		targetHash="${Repo_Target_Hash[${repo}]}"
 		if [ "${targetHash}" == "None" ] ; then
-			cd "${WorkDir}"
 			continue
 		fi
 
-		cd ${Repo_Directory[${repo}]}
-		theHash="$(git rev-parse HEAD)"
+		theHash="$( cd ${parentDir}/${Repo_Directory[${repo}]} && git rev-parse HEAD )"
 		if [ "${theHash}" != "${targetHash}" ] ; then
-			COM="git revert ${targetHash}"
+			COM="( cd ${parentDir}/${Repo_Directory[${repo}]} && git revert ${targetHash} --no-edit )"
 			rclr "Ensuring that the repo is on the target hash." "${COM}" "Target hash setting"
 		fi
         fi
-	cd "${WorkDir}"
 done
